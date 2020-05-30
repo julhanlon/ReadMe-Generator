8ea6f7048f3ac5437aa1ff993092ad43fd7ae7cb
Julia Hanlon <jhanlon289@gmail.com>
2020-05-30 15:39:00 -0700

    updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
 // const generateMD = require("./generateMarkdown");
-const gitAPi = require("./api");
+
 inquirer
   .prompt({
     type: "input",
@@ -21,44 +21,58 @@ inquirer
     });
   });
 
-inquirer.prompt([
-  {
-    type: "input",
-    message: "Enter your GitHub username:",
-    name: "username",
-  },
-  {
-    type: "input",
-    message: "Enter project title",
-    name: "title",
-  },
-  {
-    type: "input",
-    message: "Enter your project description",
-    name: "description",
-  },
-  {
-    type: "input",
-    message: "Enter Table of Contents",
-    name: "table of contents",
-  },
-  {
-    type: "input",
-    message: "What is the installation process?",
-    name: "installation",
-  },
-  {
-    type: "list",
-    message: "Choose a license",
-    name: "license",
-    choices: ["MIT", "Apache", "GPL", "Affero GPL"],
-  },
-  {
-    type: "input",
-    message: "who are the contributors",
-    name: "contributors",
-  },
-]);
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Enter your GitHub username:",
+      name: "username",
+    },
+    {
+      type: "input",
+      message: "Enter project title",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Enter your project description",
+      name: "description",
+    },
+    {
+      type: "checkbox",
+      message: "Select categories to include in Table of Contents",
+      name: "table of contents",
+      choices: [
+        "Installation",
+        "Usage",
+        "License",
+        "Contributing",
+        "Tests",
+        "Questions",
+      ],
+    },
+    {
+      type: "input",
+      message: "What is the installation process?",
+      name: "installation",
+      default: "no install",
+    },
+    {
+      type: "list",
+      message: "Choose a license",
+      name: "license",
+      choices: ["MIT", "Apache", "GPL", "Affero GPL"],
+    },
+    {
+      type: "input",
+      message: "who are the contributors",
+      name: "contributors",
+      default: "only me",
+    },
+  ])
+  .then((answers) => {
+    console.log("Answers:", answers);
+  });
 
 // inquirer.prompt(questions).then((answers) => console.log(answers));
 
