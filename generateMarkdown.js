d4b0c51fea13fc879096951c5f784b068a5d89cf
Julia Hanlon <jhanlon289@gmail.com>
2020-06-01 18:32:35 -0700

    not using generateMarkdown.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,27 +1,27 @@
-function generateMarkdown(data) {
-  return `
-# ${data.title}
+// function generateMarkdown(data) {
+//   return `
+// # ${data.title}
 
-![picture](${resp.data.avatar_url})
+// ![picture](${resp.data.avatar_url})
 
-${data.description}
-# Table of content
-- Installation
-- Usage
-- Licence
-- Contributing
-- Questions
+// ${data.description}
+// # Table of content
+// - Installation
+// - Usage
+// - Licence
+// - Contributing
+// - Questions
 
-# Installation
-${data.installation}
-# Usage
-${data.usage}
-# Licence
-${data.licence}
-# Contributors
-${data.contributors}
+// # Installation
+// ${data.installation}
+// # Usage
+// ${data.usage}
+// # Licence
+// ${data.licence}
+// # Contributors
+// ${data.contributors}
 
-`;
-}
+// `;
+// }
 
-module.exports = generateMarkdown;
+// module.exports = generateMarkdown;
