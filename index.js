ce1aaa6c721f20b23889e343d88e10d024975f83
Julia Hanlon <jhanlon289@gmail.com>
2020-06-02 21:51:54 -0700

    still wokring on it

--- index.js
+++ index.js
@@ -3,82 +3,81 @@ const fs = require("fs");
 const axios = require("axios");
 
 var userEmail = "";
-var userPic;
+var userPic = "";
 
 inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "Enter your Github username",
-      name: "username",
-    },
-    {
-      type: "input",
-      message: "Enter your project title",
-      name: "title",
-    },
-    {
-      type: "input",
-      message: "Enter your project description",
-      name: "description",
-    },
-    {
-      type: "input",
-      message: "What is the installation process?",
-      name: "installation",
-      default: "no install",
-    },
-    {
-      type: "input",
-      message: "What do you use this project for?",
-      name: "usage",
-    },
-    {
-      type: "list",
-      message: "Choose a license",
-      name: "license",
-      choices: ["MIT", "Apache", "GPL", "Affero GPL"],
-    },
-    {
-      type: "input",
-      message: "who are the contributors",
-      name: "contributors",
-      default: "only me",
-    },
-    {
-      type: "input",
-      message: "What tests would you like to run?",
-      name: "tests",
-      default: "none",
-    },
-    {
-      type: "confirm",
-      message: "Do you have any questions?",
-      name: "questionConfirm",
-    },
-    {
-      type: "confirm",
-      message: "What is your question?",
-      name: "questions",
-      when: (answers) => answers.questionConfirm === true,
-    },
-  ])
-  .then(({ username }) => {
-    axios.get(`https://api.github.com/users/${username}`).then(({ data }) => {
-      console.log(data);
-    });
+  .prompt({
+    type: "input",
+    message: "Enter your GitHub username",
+    name: "username",
   })
-  .then((res) => {
+  .then(({ username }) => {
+    axios.get(`https://api.github.com/users/${username}`);
+    return axios.get(`https://api.github.com/users/${username}`);
+  })
+  .then(function (res) {
+    userPic = res.data.avatar_url;
     userEmail = res.data.email;
     if (userEmail === null) {
-      userEmail = "No user email";
+      userEmail = "No User Email";
     }
-    userPic = res.avatar_url;
-  })
-  .then((res) => {
-    fs.writeFile(
-      "README.md",
-      `# ${res.title}
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "Enter your project title",
+          name: "title",
+        },
+        {
+          type: "input",
+          message: "Enter your project description",
+          name: "description",
+        },
+        {
+          type: "input",
+          message: "What is the installation process?",
+          name: "installation",
+          default: "no install",
+        },
+        {
+          type: "input",
+          message: "What do you use this project for?",
+          name: "usage",
+        },
+        {
+          type: "list",
+          message: "Choose a license",
+          name: "license",
+          choices: ["MIT", "Apache", "GPL", "Affero GPL"],
+        },
+        {
+          type: "input",
+          message: "who are the contributors",
+          name: "contributors",
+          default: "only me",
+        },
+        {
+          type: "input",
+          message: "What tests would you like to run?",
+          name: "tests",
+          default: "none",
+        },
+        {
+          type: "confirm",
+          message: "Do you have any questions?",
+          name: "questionConfirm",
+        },
+        {
+          type: "input",
+          message: "What is your question?",
+          name: "questions",
+          when: (answers) => answers.questionConfirm === true,
+        },
+      ])
+      .then((res) => {
+        fs.writeFile(
+          "README.md",
+          `# ${res.title}
       ***
     ![license](https://img.shields.io/badge/License-${res.license}-blue)
       ***
@@ -114,6 +113,7 @@ inquirer
     ***
     Email:${userEmail}
     `
-    );
+        );
+      });
   })
   .catch((error) => console.log(error));
