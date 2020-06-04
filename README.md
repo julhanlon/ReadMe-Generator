e6fee23ccd340ab7b5aaeab988958d48ce2cf684
Julia Hanlon <jhanlon289@gmail.com>
2020-06-03 18:33:11 -0700

    works now!

--- README.md
+++ README.md
@@ -1,36 +1,36 @@
-# undefined
-      ***
-    ![license](https://img.shields.io/badge/License-undefined-blue)
-      ***
-    ![userPic](https://avatars0.githubusercontent.com/u/59458162?v=4)
-    ***
-    ![giphy]("./")
-    ***
-    ## Table of Contents
+# Readme Generator
+      
+![license](https://img.shields.io/badge/License-MIT-blue)
+
+![userPic](https://avatars0.githubusercontent.com/u/59458162?v=4)
+    
+![giphy]("./")
+    
+## Table of Contents
     - Description
     - Installation
     - Usage
     - Contributors 
     - Tests
     - Questions
-    ***
-    ## Description
+    
+## Description
     undefined
-    ***
-    ## How to install
+    
+## How to install
     undefined
-    ***
-    ## Intended Usage
+
+## Intended Usage
+    to create a readme file for github
+
+## Contributors
+    only me
+    
+## Tests
+    none
+  
+## Questions
     undefined
-    ***
-    ## Contributors
-    undefined
-    ***
-    ## Tests
-    undefined
-    ***
-    ## Questions
-    undefined
-    ***
-    Email:
+  
+Email:null
     
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+//loading modules into the file
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
 
+//section for variables
 var userEmail = "";
 var userPic = "";
+var userInput = {};
 
+//section for questions using inquirer
 inquirer
   .prompt([
     { type: "input", message: "Enter your GitHub username", name: "username" },
@@ -56,81 +60,65 @@ inquirer
       type: "input",
       message: "What is your question?",
       name: "questions",
+      default: "no questions",
       when: (answers) => answers.questionConfirm === true,
     },
   ])
-  .then(function ({ username }) {
-    const gitUrl = `https://api.github.com/users/${username}`;
-    const emailUrl = `https://api.github.com/users/${username}/events/public`;
-    axios.get(gitUrl).then((res) => {
-      userPic = res.data.avatar_url;
-      axios
-        .get(emailUrl)
-        .then((res) => {
-          userEmail = res.data.email;
-          if (userEmail === null) {
-            userEmail = "No user email";
-          }
-        })
-        .then((res) => {
-          console.log(res.title);
-          console.log(res.description);
-          console.log(res.installation);
-          console.log(res.usage);
-          console.log(res.license);
-          console.log(res.contributors);
-          console.log(res.tests);
-          console.log(res.questionConfirm);
-          console.log(res.questions);
-          console.log(res.data.email);
-          console.log(res.data.avatar_url);
-        });
-      fs.writeFile(
-        "README.md",
-        `# ${res.title}
-      ***
-    ![license](https://img.shields.io/badge/License-${res.license}-blue)
-      ***
-    ![userPic](${userPic})
-    ***
-    ![giphy]("./")
-    ***
-    ## Table of Contents
+  .then((answers) => {
+    userInput = answers;
+    console.log("Answer:", answers);
+    axios
+      .get(`https://api.github.com/users/${answers.username}`)
+      .then((response) => {
+        console.log(response.data);
+        userEmail = response.data.email;
+        userPic = response.data.avatar_url;
+        console.log(userEmail, userPic);
+        fs.writeFile(
+          "README.md",
+          `# ${answers.title}
+      
+![license](https://img.shields.io/badge/License-${answers.license}-blue)
+
+![userPic](${userPic})
+    
+![giphy]("./")
+    
+## Table of Contents
     - Description
     - Installation
     - Usage
     - Contributors 
     - Tests
     - Questions
-    ***
-    ## Description
-    ${res.descriptionl}
-    ***
-    ## How to install
-    ${res.install}
-    ***
-    ## Intended Usage
-    ${res.usage}
-    ***
-    ## Contributors
-    ${res.contributors}
-    ***
-    ## Tests
-    ${res.tests}
-    ***
-    ## Questions
-    ${res.questions}
-    ***
-    Email:${userEmail}
+    
+## Description
+    ${answers.descriptionl}
+    
+## How to install
+    ${answers.install}
+
+## Intended Usage
+    ${answers.usage}
+
+## Contributors
+    ${answers.contributors}
+    
+## Tests
+    ${answers.tests}
+  
+## Questions
+    ${answers.questions}
+  
+Email:${userEmail}
     `,
-        (error) => {
-          if (error) {
-            console.log(error);
-          } else {
-            console.log("Sucess!");
+          (error) => {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log("Sucess!");
+            }
           }
-        }
-      );
-    });
-  })
-  .catch((error) => console.log(error));
+        );
+      });
+  });
